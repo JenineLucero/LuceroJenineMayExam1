161a436ba646509abe611e2ff6175fce2c507df4
unknown <LUCERO_JENINE_115@IICSGENERAL.COM>
2018-09-18 14:58:30 +0800

    Initial commit

---
+++ app/src/main/java/com/lucero/jeninemay/Exam1.java
@@ -0,0 +1,13 @@
+package com.lucero.jeninemay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exam1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exam1);
+    }
+}
